482497421ee04f619586458d0134d12ffbb29a40
cprice25 <98678511+cprice25@users.noreply.github.com>
2024-04-09 11:26:20 -0400

    Update DataEntry.java
    added comments

--- src/main/java/org/openjfx/Workflow/DataEntry.java
+++ src/main/java/org/openjfx/Workflow/DataEntry.java
@@ -1,22 +1,17 @@
 package org.openjfx.Workflow;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import org.openjfx.Business.Form;
 import org.openjfx.Business.FormStatus;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
 
 public class DataEntry {
@@ -27,9 +22,11 @@ public class DataEntry {
     public Scene dataEntryScene;
 
     public void dataEntryScene(Form form, Workflow system, Stage primaryStage) {
+        //initialize form and the workflow
         this.systemForm = form;
         this.DataEntryWorkflow = system;
-        Button submit = new Button();
+
+        Button submit = new Button(); //submit button
         submit.setText("Submit Data Entry Form");
         GridPane layout = new GridPane();
         layout.setAlignment(Pos.CENTER);
@@ -39,26 +36,30 @@ public class DataEntry {
              "Birth Day: ", "Birth Year: ","SS Number: ", "Immigrant PID: ", "Dependent PID: ", "Race: ",
             "Gender: ", "Married: ", "Phone Number: ", "Father's Name: ", "Mother's Name: ",
             "Address: ", "Employment Status: "};
+        
         String [] format = {"John", "Carlton", "Doe", "25", "06", "03",
             "1999", "222880000", "1893", "5833", "Caucasion", "Male",
             "True", "8889563400", "Jason Doe", "Katherine Doe", 
             "4400 University Dr, Fairfax, VA 22030", "True" };
         String prompt = "Enter ";
         
+        //col and row to set the position to place the nodes on the gridpane
         int col =0;
         int row = 0;
+
+        //arraylist of textfields to keep track of later
         ArrayList<TextField> fields = new ArrayList<>();
-        for (int j =0; j<2; j++){
+
+        for (int j =0; j<2; j++){ // the first iteration will be for the immigrant and second iteration will be for dependent
             String person = "Immigrant ";
-            if (j == 1){
+            if (j == 1){ //second iteration
                 person = "Dependent ";
             }
-        for (int i=0; i<label_names.length; i++){
+        for (int i=0; i<label_names.length; i++){ //match the label names with textFields
 
             Label label = new Label(person + label_names[i]);
             TextField textField= new TextField ();
-            textField.setPromptText(prompt+label_names[i] + " ex: " + format[i]);
-            //textField.setMaxWidth(300);
+            textField.setPromptText(prompt+label_names[i] + " ex: " + format[i]);//set the prompt for the textfield as a guide to the user
             
            //3 items per row
             if (i%3 ==0){
@@ -67,32 +68,34 @@ public class DataEntry {
             }
             layout.add(label, row++, col);
             layout.add(textField, row++, col);
-            fields.add(textField);
-            if ((j ==1) && ((i== label_names.length-1))){
+            fields.add(textField); //add the textfield to the arrayList of textfields
+            if ((j ==1) && ((i== label_names.length-1))){ //if on the last iteration and for dependent, add previously claimed
                 Label label2 = new Label(person + "Previously Claimed: ");
                 TextField textField2= new TextField ();
                 textField2.setPromptText("Previously Claimed: " + " ex: " + "True");
-                //textField2.setMaxWidth(300);
                 fields.add(textField2);
-                
                 layout.add(label2, 0, ++col);
                 layout.add(textField2, 1, col);
             }
             
         }
     }
-
-
         dataEntryScene = new Scene(layout, 1170, 700);
         dataEntryScene.getRoot().setStyle("-fx-font-family: 'serif'");
         primaryStage.setScene(dataEntryScene);
-        //systemForm.setDependent(systemForm.getImmigrant().getDependent());
         
+        /*On pressing the submit button do this .... */
         submit.setOnAction(e -> {
-            systemForm.updateStatus(FormStatus.INPROGRESS);
-            for (int i = 0; i<fields.size(); i++){
+            systemForm.updateStatus(FormStatus.INPROGRESS); //form in progress but not complete
+            for (int i = 0; i<fields.size(); i++){ //iterate over arrayList of textfields
                 switch (i){
-                    /*Immigrant info */
+                    /*Immigrant info
+                     * get the immigrant associated with the form
+                     * set each member of the immigrant class by accessing the arraylist of created fields,
+                     *      and iterating over the arraylist and at each field, retrieve the string text. If 
+                     *      necessary parse the information into an Integer or Boolean so that it matches the 
+                     *      arguments of the called method. 
+                     */
                     case 0: systemForm.getImmigrant().setFirstName(fields.get(i).getText()); break;
                     case 1: systemForm.getImmigrant().setMiddleName(fields.get(i).getText()); break;
                     case 2: systemForm.getImmigrant().setLastName(fields.get(i).getText()); break;
@@ -112,7 +115,13 @@ public class DataEntry {
                     case 16: systemForm.getImmigrant().setAddress(fields.get(i).getText());
                     case 17: systemForm.getImmigrant().setemploymentStatus(Boolean.parseBoolean(fields.get(i).getText())); break;
                     
-                   // Dependent info 
+                   /*Dependent info
+                     * get the immigrant associated with the form
+                     * set each member of the immigrant class by accessing the arraylist of created fields,
+                     *      and iterating over the arraylist and at each field, retrieve the string text. If 
+                     *      necessary parse the information into an Integer or Boolean so that it matches the 
+                     *      arguments of the called method. 
+                     */
                     case 18: systemForm.getDependent().setFirstName(fields.get(i).getText()); break;
                     case 19: systemForm.getDependent().setMiddleName(fields.get(i).getText()); break;
                     case 20: systemForm.getDependent().setLastName(fields.get(i).getText()); break;
@@ -132,10 +141,10 @@ public class DataEntry {
                     case 34: systemForm.getDependent().setAddress(fields.get(i).getText()); break;
                     case 35: systemForm.getDependent().setemploymentStatus(Boolean.parseBoolean(fields.get(i).getText())); break;
                     case 36: systemForm.getDependent().setPrevClaim(Boolean.parseBoolean(fields.get(i).getText())); 
-                    systemForm.updateStatus(FormStatus.COMPLETE);break;
+                    systemForm.updateStatus(FormStatus.COMPLETE);break; // the form should be complete if gotten this far
                     
                 }}
-            DataEntryWorkflow.getReview().rDisplay(systemForm, DataEntryWorkflow, primaryStage);
+            DataEntryWorkflow.getReview().rDisplay(systemForm, DataEntryWorkflow, primaryStage); //hand the form off to the review stage
             primaryStage.setScene(DataEntryWorkflow.getReview().rScene);
         });
 
